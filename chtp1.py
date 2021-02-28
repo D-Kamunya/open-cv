205914e0f950de0bd2f849a8e83460b1ade5470a
Dennis Kamunya <kinc.developer@gmail.com>
2021-02-28 21:03:54 +0300

    Import and display videos using opencv

--- chtp1.py
+++ chtp1.py
@@ -1,7 +1,18 @@
 import cv2
 
-Display images
-img = cv2.imread("Resources/wiz.jpg")
+# Display images
+# img = cv2.imread("Resources/wiz.jpg")
 
-cv2.imshow("Wiz",img)
-cv2.waitKey(0)
+# cv2.imshow("Wiz",img)
+# cv2.waitKey(0)
+
+# Display videos
+cap=cv2.VideoCapture("Resources/test.mp4")
+
+while True:
+  success, img = cap.read()
+  cv2.imshow("video",img)
+  if cv2.waitKey(1) & 0xFF ==ord('q'):
+    break
+
+
