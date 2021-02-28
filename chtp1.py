5fa4f15bf02014d6fdfcc1fb8990d24c23c5d9d1
Dennis Kamunya <kinc.developer@gmail.com>
2021-02-28 21:04:41 +0300

    Display from webcam  using opencv

--- chtp1.py
+++ chtp1.py
@@ -7,12 +7,22 @@ import cv2
 # cv2.waitKey(0)
 
 # Display videos
-cap=cv2.VideoCapture("Resources/test.mp4")
+# cap=cv2.VideoCapture("Resources/test.mp4")
+
+# while True:
+#   success, img = cap.read()
+#   cv2.imshow("video",img)
+#   if cv2.waitKey(1) & 0xFF ==ord('q'):
+#     break
+
+# Using webcam
+cap=cv2.VideoCapture(0)
+cap.set(3,640) # Set width id no is 3
+cap.set(4,480) # set height id no is 4
+cap.set(10,100) # Define the brightness
 
 while True:
   success, img = cap.read()
   cv2.imshow("video",img)
   if cv2.waitKey(1) & 0xFF ==ord('q'):
     break
-
-
