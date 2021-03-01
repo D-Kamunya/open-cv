55dd07fcfedb0ec03a1f6a6233c9c3682618361d
Dennis Kamunya <kinc.developer@gmail.com>
2021-03-01 17:18:59 +0300

    Convert image to grayscale

--- chtp1.py
+++ chtp1.py
@@ -16,13 +16,21 @@ import cv2
 #     break
 
 # Using webcam
-cap=cv2.VideoCapture(0)
-cap.set(3,640) # Set width id no is 3
-cap.set(4,480) # set height id no is 4
-cap.set(10,100) # Define the brightness
+# cap=cv2.VideoCapture(0)
+# cap.set(3,640) # Set width id no is 3
+# cap.set(4,480) # set height id no is 4
+# cap.set(10,100) # Define the brightness
 
-while True:
-  success, img = cap.read()
-  cv2.imshow("video",img)
-  if cv2.waitKey(1) & 0xFF ==ord('q'):
-    break
+# while True:
+#   success, img = cap.read()
+#   cv2.imshow("video",img)
+#   if cv2.waitKey(1) & 0xFF ==ord('q'):
+#     break
+
+img = cv2.imread("Resources/wiz.jpg")
+
+# Convert image to grayscale (2 params;img to be converted and color space)
+imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Image",imgGray)
+cv2.waitKey(0)
+0xFF ==ord('q')
