0935b0f89214a50780bab5aaab5d9631033b0402
Dennis Kamunya <kinc.developer@gmail.com>
2021-03-02 20:46:00 +0300

    Add/draw a line on an image

--- compv2.py
+++ compv2.py
@@ -6,7 +6,10 @@ img=np.zeros((512,512))
 #Add channels
 img=np.zeros((512,512,3),np.uint8)
 #Add color
-img[200:400,200:400]=255,0,0
+# img[200:400,200:400]=255,0,0
+
+# Add line
+cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)#3 is for thickness (W,H)
 cv2.imshow("Image",img)
 
 cv2.waitKey(0)
